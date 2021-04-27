a452d7a03b464661f4ed66ab743f5567b7847f43
LuciLua <luci.lua81@gmail.com>
2021-04-27 01:50:37 -0300

    Calendar :calendar:

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ import ptBR from 'date-fns/locale/pt-BR';
 
 const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
   locale: ptBR,
-})
+}) 
 
 type Usuarios = {
   id: number;
@@ -30,8 +30,56 @@ export default function Home() {
       <title>Calendário</title>
    </Head>
     <div className={styles.header}>
-      <h1>Calendário!</h1>
-      <span>{currentDate}</span>
+      <span>{currentDate} <p></p> 2021</span>
+    </div>
+    <div className={styles.calendar}>
+      <table>
+        <tr className={styles.Week}>
+          <th>Domingo</th>
+          <th>Segunda</th>
+          <th>Terça</th>
+          <th>Quarta</th>
+          <th>Quinta</th>
+          <th>Sexta</th>
+          <th>Sábado</th>
+        </tr>
+        <tr className={styles.days}>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+          <td>6</td>
+          <td>7</td>
+        </tr>
+        <tr>
+          <td>8</td>
+          <td>9</td>
+          <td>10</td>
+          <td>11</td>
+          <td>12</td>
+          <td>13</td>
+          <td>14</td>
+        </tr>
+        <tr>
+          <td>15</td>
+          <td>16</td>
+          <td>17</td>
+          <td>18</td>
+          <td>19</td>
+          <td>20</td>
+          <td>21</td>
+          </tr>
+          <tr>
+          <td>22</td>
+          <td>23</td>
+          <td>24</td>
+          <td>25</td>
+          <td>26</td>
+          <td>27</td>
+          <td>28</td>
+          </tr>
+      </table>
     </div>
    </div>
   )
